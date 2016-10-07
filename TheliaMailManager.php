e06f1dc22a01a8f12d7a6a84c5587ef2b99bc1e6
gilles <gilles.bourgeat@gmail.com>
2016-10-07 20:03:29 +0200

    First commit

---
+++ TheliaMailManager.php
@@ -0,0 +1,28 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace TheliaMailManager;
+
+use Thelia\Module\BaseModule;
+
+class TheliaMailManager extends BaseModule
+{
+    /** @var string */
+    const DOMAIN_NAME = 'theliamailmanager';
+
+    /*
+     * You may now override BaseModuleInterface methods, such as:
+     * install, destroy, preActivation, postActivation, preDeactivation, postDeactivation
+     *
+     * Have fun !
+     */
+}
