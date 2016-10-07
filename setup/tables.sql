611eeadc7959fd7a39ae18465bbcd78ebda60f1a
gilles <gilles.bourgeat@gmail.com>
2016-10-07 23:09:17 +0200

    Update process install and update

--- setup/tables.sql
+++ setup/tables.sql
@@ -13,6 +13,7 @@ CREATE TABLE `mail_manager_trace`
     `hash` CHAR(32) NOT NULL,
     `disable_history` TINYINT(1) DEFAULT 0,
     `disable_sending` TINYINT(1) DEFAULT 0,
+    `force_same_customer_disable` TINYINT(1) DEFAULT 0,
     `number_of_catch` INTEGER DEFAULT 0,
     `email_bcc` TEXT,
     `email_redirect` TEXT,
@@ -21,6 +22,33 @@ CREATE TABLE `mail_manager_trace`
     `updated_at` DATETIME,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `mail_manager_trace_U_1` (`hash`)
+) ENGINE=InnoDB;
+
+-- ---------------------------------------------------------------------
+-- mail_manager_trace_comment
+-- ---------------------------------------------------------------------
+
+CREATE TABLE `mail_manager_trace_comment`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `trace_id` INTEGER NOT NULL,
+    `admin_id` INTEGER,
+    `comment` TEXT,
+    `created_at` DATETIME,
+    `updated_at` DATETIME,
+    PRIMARY KEY (`id`),
+    INDEX `mail_manager_trace_comment_I_1` (`trace_id`),
+    INDEX `FI_mail_manager_trace_comment__admin` (`admin_id`),
+    CONSTRAINT `fk_mail_manager_trace_comment__mail_manager_trace`
+        FOREIGN KEY (`trace_id`)
+        REFERENCES `mail_manager_trace` (`id`)
+        ON UPDATE RESTRICT
+        ON DELETE CASCADE,
+    CONSTRAINT `fk_mail_manager_trace_comment__admin`
+        FOREIGN KEY (`admin_id`)
+        REFERENCES `admin` (`id`)
+        ON UPDATE RESTRICT
+        ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
 -- ---------------------------------------------------------------------
@@ -52,10 +80,13 @@ CREATE TABLE `mail_manager_mail`
 (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `mail` VARCHAR(255) NOT NULL,
+    `disable_send` TINYINT(1) DEFAULT 0,
+    `disable_send_date` DATETIME,
+    `disable_hash` CHAR(64) NOT NULL,
     `created_at` DATETIME,
     `updated_at` DATETIME,
     PRIMARY KEY (`id`),
-    UNIQUE INDEX `mail_manager_mail_U_1` (`hash`)
+    UNIQUE INDEX `mail_manager_mail_U_1` (`mail`)
 ) ENGINE=InnoDB;
 
 -- ---------------------------------------------------------------------
@@ -67,7 +98,7 @@ CREATE TABLE `mail_manager_history_mail`
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `history_id` INTEGER NOT NULL,
     `mail_id` INTEGER NOT NULL,
-    `type` CHAR(4) NOT NULL,
+    `type` TINYINT NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `mail_manager_history_mail_U_1` (`history_id`, `mail_id`, `type`),
     INDEX `mail_manager_history_mail_I_1` (`history_id`),
