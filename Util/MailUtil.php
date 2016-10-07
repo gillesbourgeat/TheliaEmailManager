bacf418e35b14aa9f8783c356fd060028b005169
gilles <gilles.bourgeat@gmail.com>
2016-10-07 20:59:23 +0200

    Add MailUtil

---
+++ Util/MailUtil.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TheliaMailManager\Util;
+
+/**
+ * Class MailUtil
+ * @package TheliaMailManager\Util
+ * @author Gilles Bourgeat <gilles.bourgeat@gmail.com>
+ */
+class MailUtil
+{
+    /**
+     * for check if the email is valid
+     * @param string $mail
+     * @return bool
+     */
+    public static function checkMailStructure($mail)
+    {
+        return filter_var($mail, FILTER_VALIDATE_EMAIL);
+    }
+
+    /**
+     * @param string $mail Check DNS records corresponding to a given Internet host name
+     * @return bool true if any records are found; returns false if no records
+     * were found or if an error occurred.
+     */
+    public static function checkMailDNS($mail)
+    {
+        if (static::checkMailStructure($mail)) {
+            $domain = explode('@', $mail);
+
+            return dns_check_record($domain, 'MX');
+        }
+
+        return false;
+    }
+}
