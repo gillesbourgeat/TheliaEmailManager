38dad98aef507f5b909559cc0699b7e35984c352
gilles <gilles.bourgeat@gmail.com>
2016-10-08 12:32:08 +0200

    Add base for routing and controller

---
+++ Controller/ConfigController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TheliaMailManager\Controller;
+
+use Thelia\Controller\Admin\BaseAdminController;
+use Thelia\Core\HttpFoundation\Request;
+
+/**
+ * @author Gilles Bourgeat <gilles.bourgeat@gmail.com>
+ */
+class ConfigController extends BaseAdminController
+{
+    public function viwAction(Request $request)
+    {
+
+    }
+
+    public function updateAction(Request $request)
+    {
+
+    }
+}
---
+++ Controller/TraceController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TheliaMailManager\Controller;
+
+use Thelia\Controller\Admin\BaseAdminController;
+use Thelia\Core\HttpFoundation\Request;
+
+/**
+ * @author Gilles Bourgeat <gilles.bourgeat@gmail.com>
+ */
+class TraceController extends BaseAdminController
+{
+    public function listAction(Request $request)
+    {
+
+    }
+
+    public function viwAction(Request $request, $traceId)
+    {
+
+    }
+
+    public function updateAction(Request $request, $traceId)
+    {
+
+    }
+}
