1c9c016365367ce8fd608381c0e5f07f7b4981ee
gilles <gilles.bourgeat@gmail.com>
2016-11-04 16:00:09 +0100

    Update i18n

--- I18n/frontOffice/default/fr_FR.php
+++ I18n/frontOffice/default/fr_FR.php
@@ -1,7 +1,9 @@
 <?php
 
 return array(
+    'Confirm Disable !' => 'Confirmer la désactivation !',
     'Desactivate email sending' => 'Désactiver l\'envoi d\'e-mails',
+    'Do you really want to stop receiving email from this site on the address : %email' => 'Voulez-vous vraiment ne plus recevoir d\'e-mail en provenance de ce site sur l\'e-mail : %email',
     'Sorry ! This link is more valid for deactivate the emails sending.' => 'Pardon ! Ce lien n\'est plus valable pour désactiver l\'envoi d\'e-mail.',
     'The sending email has been deactivated for the email : %email' => 'L\'envoi d\'email a été désactivé pour l\'e-mail : %email',
     'View my account !' => 'Voir mon compte !',
