d1f441f641c6811a00c46d980d617f1be3058c33
gilles <gilles.bourgeat@gmail.com>
2016-11-15 00:10:38 +0100

    Add license

--- Controller/Front/EmailController.php
+++ Controller/Front/EmailController.php
@@ -1,4 +1,12 @@
 <?php
+/*************************************************************************************/
+/*      Copyright (C) 2016 Gilles Bourgeat <gilles.bourgeat@gmail.com>               */
+/*                                                                                   */
+/*      This file is part of TheliaEmailManager module.                              */
+/*                                                                                   */
+/*      The module TheliaEmailManager can not be copied and/or distributed without   */
+/*      permission of Gilles Bourgeat.                                               */
+/*************************************************************************************/
 
 namespace TheliaEmailManager\Controller\Front;
 
