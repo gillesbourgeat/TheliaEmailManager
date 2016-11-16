228193379e18996fdfad0a548c8f038139ab1d99
gilles <gilles.bourgeat@gmail.com>
2016-11-16 16:29:16 +0100

    Fix typo

--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -12,7 +12,7 @@ return array(
     'List of emails separated by comma' => 'Une Liste d\'e-mail séparé par une virgule',
     'List of emails separated by commas' => 'Une Liste d\'e-mail séparé par une virgule',
     'No' => 'Non',
-    'The Email Manager Module detected an new trace' => 'Le module  Email Manager a détectée une nouvelle trace',
+    'The Email Manager Module detected an new trace' => 'Le module  Email Manager a détecté une nouvelle trace',
     'The emails will be send same if the user no longer wishes to receive' => 'Les e-mails seront envoyés même si l\'utilisateur ne souhaite plus en recevoir',
     'Yes' => 'Oui',
     'email1@my-domain.tld, email2@my-domain.tld' => 'e-mail1@mon-domaine.tld, e-mail2@mon-domaine.tld',
